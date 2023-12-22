f011ddae7af5e3cf87f9412852280604e542167d
Mettteora <mrhens2013@gmail.com>
2023-12-22 15:14:38 +0100

    123

---
+++ src/js/02-timer.js
@@ -0,0 +1,6 @@
+// import flatpickr from 'flatpickr';
+
+const daysSpan = document.querySelector('[data-days]');
+const hoursSpan = document.querySelector('[data-hours]');
+const minutesSpan = document.querySelector('[data-minutes]');
+const secondsSpan = document.querySelector('[data-seconds]');
